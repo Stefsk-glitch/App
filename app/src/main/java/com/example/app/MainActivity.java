9f47cca92fea5f69ac6eb8cd0ef8d1dd91c668d3
GertieMeneer <gertjan.vat@icloud.com>
2023-04-22 10:12:59 +0200

    added bootup sound

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(this, R.raw.androide);
+        mp.start();
+
     }
 }
