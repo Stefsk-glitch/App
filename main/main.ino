63ed37052903447c896954906d272207a8db9b69
GertieMeneer <gertjan.vat@icloud.com>
2023-05-25 12:41:05 +0200

    made main

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,15 @@
+#include <PubSubClient.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+#include <WiFiClientSecure.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
-#include <WiFiClientSecure.h>
-#include <PubSubClient.h>
 
 #ifndef _BV
-  #define _BV(bit) (1<<(bit))
+#define _BV(bit) (1<<(bit))
 #endif
 
 Adafruit_LEDBackpack matrix = Adafruit_LEDBackpack();
@@ -22,8 +26,8 @@ long duration = 0;
 float distanceCm = 0;
 String localMessage = "";
 
-const char* ssid = "negersex";
-const char* password = "aidsneuk";
+const char* ssid = "xxx";
+const char* password = "xxx";
 
 const char* mqttServer = "7b10c1a6effd49c798757d01597a1663.s2.eu.hivemq.cloud";
 const int mqttPort = 8883;
@@ -36,7 +40,6 @@ PubSubClient mqttClient(wifiClient);
 void setup() {
   matrix.begin(0x75);  // pass in the address
   Serial.begin(115200);
-  Serial.println("HT16K33 test");
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -66,6 +69,7 @@ void setup() {
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
   pinMode(onboardLed, OUTPUT);
 }
 
@@ -80,6 +84,7 @@ void loop() {
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
+
   distanceCm = duration * SOUND_SPEED / 2;
 
   // Check if the incoming message is not null
@@ -89,7 +94,8 @@ void loop() {
     if (localMessage == "lol") {
       // TODO: Flash pig head 3 times after show balloon and let the balloon fly up until balloon is gone
       Serial.println("CORRECT CODE!");
-    } else {
+    }
+    else {
       // make whole led matrix RED and after clear
       Serial.println("WRONG CODE!");
     }
@@ -100,29 +106,26 @@ void loop() {
   if (distanceCm < 80) {
     for (uint8_t i = 0; i < 8; i++) {
       // draw a diagonal row of pixels
-      matrix.displaybuffer[i] = _BV((counter + i) % 16) | _BV((counter + i + 8) % 16);
+      matrix.displaybuffer[i] = _BV((counter + i) % 16) | _BV((counter + i + 8) % 16)  ;
+      matrix.writeDisplay();
+      delay(100);
+      counter++;
+      if (counter >= 16) counter = 0;
     }
     // write the changes we just made to the display
-    matrix.writeDisplay();
-    delay(100);
-    counter++;
-    if (counter >= 16) counter = 0;
-  } else {
-    // Clear the display
-    for (uint8_t i = 0; i < 8; i++) {
-      matrix.displaybuffer[i] = 0;
+  }
+    else {
+      // TODO: maak dat ogen open gaan op de led matrix
+      digitalWrite(onboardLed, LOW);
+      delay(100);
     }
-    matrix.writeDisplay();
 
-    // TODO: maak dat ogen open gaan op de led matrix
-    digitalWrite(onboardLed, LOW);
-    delay(100);
+    Serial.print("Distance (cm): ");
+    Serial.println(distanceCm);
+
+    delay(1000);
   }
 
-  Serial.print("Distance (cm): ");
-  Serial.println(distanceCm);
-
-}
 
 void callback(char* topic, byte* payload, unsigned int length) {
   // handle incoming message from mqtt hivemq
