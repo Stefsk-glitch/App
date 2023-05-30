c5a5feb99055bd5a8362732229023acce0c396e7
GertieMeneer <gertjan.vat@icloud.com>
2023-05-30 12:25:29 +0200

    fix?

--- app/src/main/java/com/example/app/builders/MessageBuilder.java
+++ app/src/main/java/com/example/app/builders/MessageBuilder.java
@@ -1,32 +1,4 @@
 package com.example.app.builders;
 
-import android.util.Log;
-
 public class MessageBuilder {
-    private StringBuilder message;
-
-    public MessageBuilder() {
-        this.message = new StringBuilder();
-    }
-
-    private boolean isValidNumber(String number) {
-        try {
-            int num = Integer.parseInt(number);
-            return num >= 1 && num <= 64;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    public void add(String number) {
-        if (isValidNumber(number)) {
-            message.append(number);
-        } else {
-            Log.d("MessageBuilder", "Invalid number: " + number);
-        }
-    }
-
-    public void printMessage() {
-        Log.d("MessageBuilder", message.toString());
-    }
 }
