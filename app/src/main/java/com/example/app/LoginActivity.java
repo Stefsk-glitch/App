67a1c9ee68dc3560a8bd19c0a611aed86e3f11e6
GertieMeneer <gertjan.vat@icloud.com>
2023-05-30 11:52:26 +0200

    made login activity

---
+++ app/src/main/java/com/example/app/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.example.app;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.app.builders.MessageBuilder;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity
+{
+    private MediaPlayer mp;
+
+    private Button submitButton;
+    private TextInputEditText textNameInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        submitButton = findViewById(R.id.submitButton);
+        textNameInput = findViewById(R.id.textNameInput);
+        mp = MediaPlayer.create(this, R.raw.androide);
+        mp.start();
+
+    }
+
+    public void onSubmitButton(View v)
+    {
+        if(String.valueOf(textNameInput.getText()).equals(""))
+        {
+            Toast toast = Toast.makeText(this, "Vergeet je naam niet in te vullen!", Toast.LENGTH_LONG);
+            toast.show();
+        } else
+        {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -8,14 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mp = MediaPlayer.create(this, R.raw.androide);
-        mp.start();
+
     }
 
     public void openMapActivity(View view){
