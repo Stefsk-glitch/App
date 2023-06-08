b6393dec7b7791b4c699eaab81c2f1d23813a040
GertieMeneer <gertjan.vat@icloud.com>
2023-06-08 11:08:34 +0200

    made map zoomable

--- app/src/main/java/com/example/app/MapActivity.java
+++ app/src/main/java/com/example/app/MapActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MapActivity extends AppCompatActivity {
+    private ImageView mapView;
+    private Button button;
+    private boolean zoomed = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +20,23 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
     }
 
-    public void openMainActivity(View view){
+    public void openMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void onEnlargeButton(View v) {
+        mapView = findViewById(R.id.mapImageView);
+        button = findViewById(R.id.enlargeButton);
+        if (zoomed) {
+            mapView.setImageResource(R.drawable.esstelingmap);
+            button.setText("Groter Maken");
+            zoomed = false;
+        } else {
+            mapView.setImageResource(R.drawable.esstelingmapzoom);
+            button.setText("Kleiner Maken");
+            zoomed = true;
+        }
+
     }
 }
